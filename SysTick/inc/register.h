8a7cda1ae65d35977a3603c6c34c7a3e0313bb5f
tsengs0 <tsengs0@gmail.com>
2019-02-12 21:43:52 +0900

    Create one register head fle for declaring/linking all registers in the processor

---
+++ SysTick/inc/register.h
@@ -0,0 +1,9 @@
+#ifndef __REGISTER_H
+#define __REGISTER_H
+
+// The special function register used for configuring the system tick
+SReg_32 *SysTick_CTRL;
+SReg_32 *SysTick_VAL;
+SReg_32 *SysTick_LOAD;
+
+#endif // __REGISTER_H
